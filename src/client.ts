faa1abf88e3bbc359f902a8a77c50bd2c9d5791a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-12-13 11:00:50 +0100

    Get  height

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,22 @@
 import { httpie } from "@arkecosystem/core-utils";
 import { Peer } from "./types";
 
-export const getWalletNonce = async (peer: Peer, walletAddress: string): Promise<string> => {
+export const getWalletNonce = async (peer: Peer, walletAddress: string): Promise<number> => {
     try {
         const response = await httpie.get(`${getServerUrl(peer)}/api/wallets/${walletAddress}`);
-        return response.body.data.nonce;
+        return parseInt(response.body.data.nonce);
     } catch (err) {
         console.error(`Cannot find wallet by address ${walletAddress}: ${err.message}`);
+        throw err;
+    }
+};
+
+export const getHeight = async (peer: Peer): Promise<number> => {
+    try {
+        const response = await httpie.get(`${getServerUrl(peer)}/api/blockchain`);
+        return parseInt(response.body.data.block.height);
+    } catch (err) {
+        console.error(`Cannot get height: ${err.message}`);
         throw err;
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import * as Loader from "./loader";
 import * as Client from "./client";
 import { Identities, Managers } from "@arkecosystem/crypto";
 
-const config = Loader.loadConfig();
-const cryptoConfig = Loader.loadCryptoConfig();
-
-Managers.configManager.setConfig(cryptoConfig);
-
-console.log(config);
-console.log(cryptoConfig);
-
 const main = async () => {
+    const config = Loader.loadConfig();
+    const cryptoConfig = Loader.loadCryptoConfig();
+
+    Managers.configManager.setConfig(cryptoConfig);
+    Managers.configManager.setHeight(await Client.getHeight(config.peer));
+
     console.log(await Client.getWalletNonce(config.peer, Identities.Address.fromPassphrase(config.senderPassphrase)));
 };
 
