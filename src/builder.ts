bdd16577ebf8cee747d34c9ea0deed02549d741b
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-01-18 12:47:13 +0900

    add all tx builders

--- src/builder.ts
+++ src/builder.ts
@@ -1,5 +1,11 @@
 import { Contracts, Identifiers } from "@mainsail/contracts";
 import { TransferBuilder } from "@mainsail/crypto-transaction-transfer";
+import { VoteBuilder } from "@mainsail/crypto-transaction-vote";
+import { UsernameRegistrationBuilder } from "@mainsail/crypto-transaction-username-registration";
+import { UsernameResignationBuilder } from "@mainsail/crypto-transaction-username-resignation";
+import { MultiPaymentBuilder } from "@mainsail/crypto-transaction-multi-payment";
+import { ValidatorRegistrationBuilder } from "@mainsail/crypto-transaction-validator-registration";
+import { ValidatorResignationBuilder } from "@mainsail/crypto-transaction-validator-resignation";
 import * as Client from "./client";
 import { Config } from "./types";
 import { getApplication } from "./boot";
@@ -25,6 +31,150 @@ export const makeTransfer = async (config: Config): Promise<Contracts.Crypto.Tra
         .vendorField(transfer.vendorField)
         .sign(senderPassphrase);
 
+
+    return signed.build();
+}
+
+export const makeVote = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { vote, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    let builder = app
+        .resolve(VoteBuilder)
+        .fee(vote.fee)
+        .nonce((walletNonce + 1).toFixed(0));
+
+    if (vote.voteAsset) {
+        builder = builder.votesAsset([vote.voteAsset]);
+    }
+
+    if (vote.unvoteAsset) {
+        builder = builder.unvotesAsset([vote.unvoteAsset]);
+    }
+
+    const signed = await builder
+        .sign(senderPassphrase);
+
+    return signed.build();
+}
+
+export const makeUsernameRegistration = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { userNameRegistration, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    const signed = await app
+        .resolve(UsernameRegistrationBuilder)
+        .fee(userNameRegistration.fee)
+        .nonce((walletNonce + 1).toFixed(0))
+        .usernameAsset(userNameRegistration.username)
+        .sign(senderPassphrase);
+
+    return signed.build();
+}
+
+export const makeUsernameResignation = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { userNameResignation, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    const signed = await app
+        .resolve(UsernameResignationBuilder)
+        .fee(userNameResignation.fee)
+        .nonce((walletNonce + 1).toFixed(0))
+        .sign(senderPassphrase);
+
+    return signed.build();
+}
+
+export const makeMultiPayment = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { multiPayment, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    let builder = app
+        .resolve(MultiPaymentBuilder)
+        .fee(multiPayment.fee)
+        .nonce((walletNonce + 1).toFixed(0));
+
+    for (const { amount, recipientId } of multiPayment.payments) {
+        builder = builder.addPayment(recipientId, amount);
+    }
+
+    const signed = await builder
+        .sign(senderPassphrase);
+
+    return signed.build();
+}
+
+export const makeValidatorRegistration = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { validatorRegistration, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    const signed = await app
+        .resolve(ValidatorRegistrationBuilder)
+        .fee(validatorRegistration.fee)
+        .nonce((walletNonce + 1).toFixed(0))
+        .publicKeyAsset(validatorRegistration.validatorPublicKey)
+        .sign(senderPassphrase);
+
+    return signed.build();
+}
+
+export const makeValidatorResignation = async (config: Config): Promise<Contracts.Crypto.Transaction> => {
+    const { cli } = config;
+    const { validatorResignation, peer, senderPassphrase } = cli;
+
+    const app = await getApplication(config);
+    const { publicKeyFactory } = makeIdentityFactories(app);
+
+    const senderPublicKey = await publicKeyFactory.fromMnemonic(senderPassphrase);
+
+    const walletNonce = await Client.getWalletNonce(peer, senderPublicKey);
+    console.log(`>> using wallet: ${senderPublicKey} nonce: ${walletNonce}`);
+
+    const signed = await app
+        .resolve(ValidatorResignationBuilder)
+        .fee(validatorResignation.fee)
+        .nonce((walletNonce + 1).toFixed(0))
+        .sign(senderPassphrase);
 
     return signed.build();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import * as Loader from "./loader";
 import * as Client from "./client";
-// // @ts-ignore
-// import { TransferBuilder } from "../../mainsail/packages/crypto-transaction-transfer";
-// // @ts-ignore
-// import { MultiPaymentBuilder } from "../../mainsail/packages/crypto-transaction-multi-payment";
-// // @ts-ignore
-// import { VoteBuilder } from "../../mainsail/packages/crypto-transaction-vote/distribution";
-// // @ts-ignore
-// import { MultiSignatureBuilder } from "../../mainsail/packages/crypto-transaction-multi-signature-registration";
-import { makeTransfer } from "./builder";
+import * as Builder from "./builder";
 
 const main = async () => {
     const config = Loader.loadConfig();
@@ -17,8 +9,7 @@ const main = async () => {
     const latestHeight = await Client.getHeight(peer);
     console.log(`>> latest height: ${latestHeight}`);
 
-
-    const tx = await makeTransfer(
+    const tx = await Builder.makeTransfer(
         config,
     );
 
@@ -27,51 +18,3 @@ const main = async () => {
 };
 
 main();
-
-// const builder = app
-//     .resolve(MultiPaymentBuilder)
-//     .network(config.crypto.network.pubKeyHash)
-//     .fee("10000000")
-//     .vendorField(Buffer.from("a".repeat(255), "utf8").toString("utf8"))
-//     .nonce((walletNonce + 1).toFixed(0));
-
-// for (let i = 0; i < 125; i++) {
-//     builder.addPayment(recipientAddress, `${i + 1}000000000`)
-// }
-
-// const signed = await builder.sign(senderWallet.passphrase);
-
-// const signed = await app
-//     .resolve(TransferBuilder)
-//     .network(config.crypto.network.pubKeyHash)
-//     .fee("10000000")
-//     .nonce((walletNonce + 1).toFixed(0))
-//     .recipientId(recipientAddress)
-//     .amount("1000000000")
-//     .vendorField("Hello Mainsail")
-//     .sign(senderWallet.passphrase)
-
-// const payload = app
-//     .resolve(MultiSignatureBuilder)
-//     .network(config.crypto.network.pubKeyHash)
-//     .fee("500000000")
-//     .nonce((walletNonce + 1).toFixed(0))
-//     .recipientId(recipientAddress)
-//     .min(2)
-//     .participant(senderPublicKey)
-//     .participant(senderPublicKey2)
-//     .vendorField("Hello Mainsail");
-
-// await payload.multiSign(senderWallet.passphrase, 0);
-// await payload.multiSign(config.validators.secrets[1], 1);
-
-// await payload.sign(senderWallet.passphrase);
-
-// const signed = await app
-//     .resolve(VoteBuilder)
-//     .network(config.crypto.network.pubKeyHash)
-//     .fee("100000000")
-//     .nonce((walletNonce + 1).toFixed(0))
-//     .recipientId(recipientAddress)
-//     .votesAsset(["4724580539e22dde52d257f3e39e6fa9911659ddcc31b8b2971b5ed20e10e873"])
-//     .sign(senderWallet.passphrase)
