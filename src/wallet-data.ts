da757539ae2802143239e4d447100847d40a0f64
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-22 11:48:26 +0000

    Show PK and address

--- src/wallet-data.ts
+++ src/wallet-data.ts
@@ -1,3 +1,7 @@
+import { getApplication } from "./boot";
+import { loadConfig } from "./loader";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+
 const main = async () => {
     if (process.argv.length !== 3) {
         console.log("Please provide a wallet mnemonic as first argument");
@@ -7,6 +11,34 @@ const main = async () => {
     const mnemonic = process.argv[2];
 
     console.log("Mnemonic: ", mnemonic);
+
+    const app = await getApplication(loadConfig());
+
+    const { publicKeyFactory, addressFactory } = makeIdentityFactories(app);
+
+    console.log("Public Key: ", await publicKeyFactory.fromMnemonic(mnemonic));
+    console.log("Address: ", await addressFactory.fromMnemonic(mnemonic));
+};
+
+const makeIdentityFactories = (
+    app: Contracts.Kernel.Application,
+): {
+    addressFactory: Contracts.Crypto.AddressFactory;
+    publicKeyFactory: Contracts.Crypto.PublicKeyFactory;
+} => {
+    return {
+        addressFactory: app.getTagged<Contracts.Crypto.AddressFactory>(
+            Identifiers.Cryptography.Identity.Address.Factory,
+            "type",
+            "wallet",
+        ),
+
+        publicKeyFactory: app.getTagged<Contracts.Crypto.PublicKeyFactory>(
+            Identifiers.Cryptography.Identity.PublicKey.Factory,
+            "type",
+            "wallet",
+        ),
+    };
 };
 
 main();
