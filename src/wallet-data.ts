c6d674c462805d7bc31580a39802c7b54c87c744
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-22 12:04:36 +0000

    Generate random mnemonic

--- src/wallet-data.ts
+++ src/wallet-data.ts
@@ -1,21 +1,16 @@
 import { getApplication } from "./boot";
 import { loadConfig } from "./loader";
 import { makeIdentityFactories } from "./builder";
+import { generateMnemonic } from "bip39";
 
 const main = async () => {
-    if (process.argv.length !== 3) {
-        console.log("Please provide a wallet mnemonic as first argument");
-        return;
-    }
-
-    const mnemonic = process.argv[2];
-
-    console.log("Mnemonic: ", mnemonic);
+    const mnemonic = process.argv.length === 3 ? process.argv[2] : generateMnemonic(256);
 
     const app = await getApplication(loadConfig());
 
     const { publicKeyFactory, addressFactory } = makeIdentityFactories(app);
 
+    console.log("Mnemonic: ", mnemonic);
     console.log("Public Key: ", await publicKeyFactory.fromMnemonic(mnemonic));
     console.log("Address: ", await addressFactory.fromMnemonic(mnemonic));
 };
