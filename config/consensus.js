52317602cea6a09278144a8e97d9fcc0b9f78bdb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-28 15:00:11 +0100

    Add transaction section

--- config/consensus.js
+++ config/consensus.js
@@ -3,6 +3,46 @@ import { ConsensusAbi } from "@mainsail/evm-contracts";
 export const consensus = {
     abi: ConsensusAbi.abi,
     contractId: "0x535B3D7A252fa034Ed71F0C53ec0C6F784cB64E1",
+    transactions: [
+        {
+            functionName: "registerValidator",
+            args: ["0".repeat(48)],
+        },
+        {
+            functionName: "updateValidator",
+            args: ["0".repeat(48)],
+        },
+        {
+            functionName: "resignValidator",
+            args: [],
+        },
+        {
+            functionName: "vote",
+            args: ["0x8233F6Df6449D7655f4643D2E752DC8D2283fAd5"],
+        },
+        {
+            functionName: "unvote",
+            args: [],
+        },
+
+        // Only for Mainsail internals
+        // {
+        //     functionName: "addValidator",
+        //     args: [],
+        // },
+        // {
+        //     functionName: "addVote",
+        //     args: [],
+        // },
+        // {
+        //     functionName: "updateVoters",
+        //     args: [],
+        // },
+        // {
+        //     functionName: "calculateActiveValidators",
+        //     args: [],
+        // },
+    ],
     views: [
         {
             functionName: "version",
@@ -38,17 +78,17 @@ export const consensus = {
         },
 
         // Only for Mainsail internals
-        {
-            functionName: "getAllValidators",
-            args: [],
-        },
-        {
-            functionName: "getVotes",
-            args: ["0x0000000000000000000000000000000000000000", 100],
-        },
-        {
-            functionName: "getRounds",
-            args: [0, 100],
-        },
+        // {
+        //     functionName: "getAllValidators",
+        //     args: [],
+        // },
+        // {
+        //     functionName: "getVotes",
+        //     args: ["0x0000000000000000000000000000000000000000", 100],
+        // },
+        // {
+        //     functionName: "getRounds",
+        //     args: [0, 100],
+        // },
     ],
 };
