1f80de81f409316823208c5caa3b119cd1db431a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-29 14:52:16 +0100

    Remove excess code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import * as Loader from "./loader.js";
 import { Contract } from "./contract.js";
 
 import { Config, ContractData } from "./types.js";
-import { Contracts } from "@mainsail/contracts";
 
 const main = async () => {
     if (process.argv.length < 3) {
@@ -18,70 +17,6 @@ const main = async () => {
     const latestHeight = await Client.getHeight(peer);
     console.log(`>> latest height: ${latestHeight}`);
 
-    // const args = process.argv.slice(2);
-
-    // let tx: Contracts.Crypto.Transaction;
-    // let txType: number;
-
-    // "transfer" "abc" "1" -- used by faucet
-    // if (args.length >= 3) {
-    //     const action = args[0];
-    //     const recipients = args[1].split(","); // comma-separated
-    //     const amount = args[2];
-    //     const functionIndex = args[3] !== undefined ? +args[3] : 0;
-
-    //     // node dist/index.js transfer "recipients" "amount"
-    //     if (!["transfer", "contract"].includes(action) || !recipients || !recipients.length || !amount) {
-    //         throw new Error("action must be 'transfer | contract' followed by the recipients and amount");
-    //     }
-
-    //     if (action === "contract") {
-    //         txType = 10;
-    //         tx = await Builder.makeEvmCall(config, functionIndex, [
-    //             functionIndex === 0 ? recipients[0] : recipients,
-    //             functionIndex === 0 ? amount : new Array(recipients.length).fill(amount),
-    //         ]);
-    //     } else {
-    //         // if (recipients.length === 1) {
-    //         txType = 1;
-    //         tx = await Builder.makeTransfer({
-    //             ...config,
-    //             cli: {
-    //                 ...config.cli,
-    //                 transfer: {
-    //                     ...config.cli.transfer,
-    //                     amount,
-    //                     recipientId: recipients[0],
-    //                 },
-    //             },
-    //         });
-    //     }
-    // } else {
-    //     txType = parseInt(process.argv[2]);
-    //     const functionIndex = parseInt(process.argv[3]) || 0;
-
-    //     if (txType === 11) {
-    //         const result = await Client.postEthView(peer, await Builder.makeEvmView(config, functionIndex));
-
-    //         Builder.decodeEvmViewResult(config, functionIndex, result);
-
-    //         return;
-    //     }
-
-    //     tx = await makeTx(txType, config, functionIndex);
-    // }
-
-    // try {
-    //     const result = await Client.postTransaction(peer, tx.serialized.toString("hex"));
-    //     console.log(`>> sent ${transactions[txType]} ${tx.id} to ${peer.apiTxPoolUrl}`);
-
-    //     console.log(result);
-    // } catch (ex) {
-    //     console.log(ex.message);
-    //     console.log(`>> failed to send tx ${tx.id} to ${peer.ip}`);
-    // }
-
-    // Later
     const txType = parseInt(process.argv[2]);
 
     switch (txType) {
@@ -143,22 +78,6 @@ const handleContract = async (config: Config, name: string, contractData: Contra
         contract.list();
     } else {
         await contract.interact(txIndex);
-    }
-};
-
-// @ts-ignore
-const makeTx = async (
-    txType: number,
-    config: Config,
-    functionIndex: number = 0,
-): Promise<Contracts.Crypto.Transaction> => {
-    switch (txType) {
-        case 1:
-            return await Builder.makeTransfer(config);
-        case 9:
-            return await Builder.makeEvmDeploy(config);
-        default:
-            throw new Error("Invalid TX type");
     }
 };
 
