495c8170dda63b5c8362df3c20ba2cb6f8d1f052
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-12-16 18:18:11 +0100

    Add two-transfers

---
+++ scripts/two-transfers.js
@@ -0,0 +1,13 @@
+import { main as getNonce } from "../dist/actions/get-nonce.js";
+import { main as exec } from "../dist/index.js";
+
+console.log("Here");
+
+const main = async () => {
+    let nonce = await getNonce();
+
+    await exec(["1", `--nonce=${nonce++}`]);
+    await exec(["1", `--nonce=${nonce++}`]);
+};
+
+main();
