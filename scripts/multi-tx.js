8e443f7067f253b8fe80bc3b1d246e4b71daccd5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-12-17 12:14:54 +0100

    Change validators

--- scripts/multi-tx.js
+++ scripts/multi-tx.js
@@ -1,20 +1,22 @@
 import { main as getNonce } from "../dist/actions/get-nonce.js";
 import { main as exec } from "../dist/index.js";
 
-import crypto from "@mainsail/core/bin/config/testnet/core/crypto.json" with { type: "json" };
-
-// const validators = [
-//     "0x8233F6Df6449D7655f4643D2E752DC8D2283fAd5",
-//     "0x2DF47283753D179929dF7FFd1f2f2f6011ef10EE",
-//     "0x3b3254D924916365A4b71aFC3291e7BcfeC03C6D",
-//     "0xC73b2668Bab0648026c3b308c9E58F7a5F9a3652",
-//     "0x950789c48D4624FF366cFD9f7E96278D92D50c11",
-//     "0x0c3345B281c3D0550c58EeBa3D3A9CAB3c77c0F4",
-//     "0x5cE62512aD72B100eeb48bC5847FaCA541b77761",
-//     "0x15026BF0Bd2DB4018FE395d329fcb67Ff634Bc1E",
-// ];
-
-const validators = crypto.genesisBlock.block.transactions.slice(0, 10).map((tx) => tx.recipientAddress);
+const validators = [
+    "0x07CDD5CB29a70ad52ba6F4ad9fb94d397a26486e",
+    "0x137c59f371a7049159ef19a72f908773Ade219b1",
+    "0x21c61c9E608df7ad6fD8730a0CEBe37A83B9F365",
+    "0xa732C81De8a23d1aa2422463Fe7489b2cBCb48c4",
+    "0x2a74550fC2e741118182B7ab020DC0B7Ed01e1db",
+    "0x7AF9d8582F439ECC9d83c84a425DFbA422bc7a84",
+    "0xb78843D7a0F237AA83911a9834Bf410C1b4c1CDe",
+    "0xA71dAdA10295c14854B5FBc6D28eFd170e8470B4",
+    "0x1Bf9cf8a006a5279ca81Ea9D3F6aC2D41e1353e2",
+    "0x07Ac3E438719be72a9e2591bB6015F10E8Af2468",
+    "0x2F61f2118fd6Fa819Cf1695642c14145a4539F9a",
+    "0x09C94A51cb63b4b70A9Dbf190543c371741D13Fd",
+    "0x373012E575CFD07dCd1139AEb3D71188677039be",
+    "0x69aD988298FB856adD785070781aaE6370cf0093",
+];
 
 const main = async () => {
     let nonce = await getNonce();
