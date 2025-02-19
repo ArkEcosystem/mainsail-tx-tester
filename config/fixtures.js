714f9f764351b5f161cdac6b436ee8dd9178647a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-02-19 09:58:45 +0000

    normalize fixture data

--- config/fixtures.js
+++ config/fixtures.js
@@ -8,11 +8,9 @@ const fixtureConfig = {
         value: "100000000",
     },
     "transfer-0": {
-        nonce: "123",
-        recipientAddress: "0xb693449adda7efc015d87944eae8b7c37eb1690a",
+        recipientAddress: "0x6F0182a0cc707b055322CcF6d4CB6a5Aff1aEb22",
     },
     "transfer-large-amount": {
-        nonce: "17",
         recipientAddress: "0x6F0182a0cc707b055322CcF6d4CB6a5Aff1aEb22",
         value: "10000000000000000000",
     },
@@ -22,14 +20,14 @@ const fixtureConfig = {
             functionName: "vote",
             args: ["0xc3bbe9b1cee1ff85ad72b87414b0e9b7f2366763"],
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
     "unvote": {
         contract: {
             data: consensus,
             functionName: "unvote",
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
     "validator-registration": {
         contract: {
@@ -37,48 +35,47 @@ const fixtureConfig = {
             functionName: "registerValidator",
             args: ["0x30954f46d6097a1d314e900e66e11e0dad0a57cd03e04ec99f0dedd1c765dcb11e6d7fa02e22cf40f9ee23d9cc1c0624"],
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
     "validator-resignation": {
         contract: {
             data: consensus,
             functionName: "resignValidator",
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
     "username-registration": {
         contract: {
             data: usernames,
             functionName: "registerUsername",
-            args: ["php"],
+            args: ["fixture"],
         },
-        gasLimit: 1000000,
-        username: "abc",
+        gasLimit: 200000,
     },
     "username-resignation": {
         contract: {
             data: usernames,
             functionName: "resignUsername",
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
     "multipayment": {
         contract: {
             data: multiPayment,
             functionName: "pay",
-            args: [["0x8233F6Df6449D7655f4643D2E752DC8D2283fAd5", "0x8233F6Df6449D7655f4643D2E752DC8D2283fAd5"], ["100000000", "200000000"]],
+            args: [["0x6F0182a0cc707b055322CcF6d4CB6a5Aff1aEb22", "0xc3bbe9b1cee1ff85ad72b87414b0e9b7f2366763"], ["100000", "200000"]],
         },
-        gasLimit: 1000000,
-        value: "300000000",
+        gasLimit: 200000,
+        value: "300000",
     },
     "multipayment-single": {
         contract: {
             data: multiPayment,
             functionName: "pay",
-            args: [["0x8233F6Df6449D7655f4643D2E752DC8D2283fAd5"], ["100000000"]],
+            args: [["0x6F0182a0cc707b055322CcF6d4CB6a5Aff1aEb22"], ["100000"]],
         },
-        gasLimit: 1000000,
-        value: "100000000",
+        gasLimit: 200000,
+        value: "100000",
     },
     "multipayment-empty": {
         contract: {
@@ -86,7 +83,7 @@ const fixtureConfig = {
             functionName: "pay",
             args: [[], []],
         },
-        gasLimit: 1000000,
+        gasLimit: 200000,
     },
 };
 
