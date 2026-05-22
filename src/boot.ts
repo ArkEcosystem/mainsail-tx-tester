be5fadd6913d2fc0acc185abef8b86a41f052b90
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-05-22 13:02:57 +0900

    fix compile errors with latest version

--- src/boot.ts
+++ src/boot.ts
@@ -1,4 +1,3 @@
-import { Container } from "@mainsail/container";
 import type { Contracts } from "@mainsail/contracts";
 import { Identifiers } from "@mainsail/constants";
 import { Application, Providers } from "@mainsail/kernel";
@@ -26,7 +25,7 @@ export const makeApplication = async (): Promise<Application> => {
         return app;
     }
 
-    app = new Application(new Container());
+    app = new Application();
 
     const plugins = [
         {
@@ -51,7 +50,7 @@ export const makeApplication = async (): Promise<Application> => {
             package: "@mainsail/crypto-address-keccak256",
         },
         {
-            package: "@mainsail/crypto-consensus-bls12-381",
+            package: "@mainsail/crypto-key-pair-bls12-381",
         },
         {
             package: "@mainsail/crypto-wif",
